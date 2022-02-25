5aa51d38532a5b0c7292f4f59d7060b22a5b7612
muhammad iqbal <muhammadiqbal16003@gmail.com>
2022-02-25 17:25:24 +0700

    update2

--- js/script.js
+++ js/script.js
@@ -1,63 +1,49 @@
 // efek scroll
-$('.page-scroll').on('click', function(e){
+$('.page-scroll').on('click', function (e) {
+  // ambil isi href
+  var href = $(this).attr('href');
+  // tangakp elemen
+  var elemenhref = $(href);
 
-    // ambil isi href
-    var href = $(this).attr('href');
-    // tangakp elemen
-    var elemenhref = $(href);
-    
-    $('html,body').animate({
-        scrollTop: elemenhref.offset().top -50
-    }, 1000, 'easeInOutBack');
+  $('html,body').animate(
+    {
+      scrollTop: elemenhref.offset().top - 50,
+    },
+    1000,
+    'easeInOutBack'
+  );
 
-    e.preventDefault();
-    
-  
-
+  e.preventDefault();
 });
 
 // paralax efect
 
 // about
-$(window).on('load', function(){
-    $('.pkiri').addClass('pmuncul')
-    $('.pkanan').addClass('pmuncul')
+$(window).on('load', function () {
+  $('.pkiri').addClass('pmuncul');
+  $('.pkanan').addClass('pmuncul');
 });
 
+$(window).scroll(function () {
+  var Wscroll = $(this).scrollTop();
+  $('.jumbotron img').css({
+    transform: 'translate(0px, ' + Wscroll / 4 + '%)',
+  });
 
+  $('.jumbotron h1').css({
+    transform: 'translate(0px, ' + Wscroll / 2 + '%)',
+  });
 
+  $('.jumbotron p').css({
+    transform: 'translate(0px, ' + Wscroll / 1.2 + '%)',
+  });
+  // portfolio
 
-
-
-$(window).scroll(function(){
-
-    var Wscroll = $(this).scrollTop();
-    $('.jumbotron img').css({
-        
-        'transform' : 'translate(0px, '+ Wscroll/4 +'%)'
+  if (Wscroll > $('.portfolio').offset().top - 250) {
+    $('.portfolio .thumbnail').each(function (i) {
+      setTimeout(function () {
+        $('.portfolio .thumbnail').eq(i).addClass('muncul');
+      }, 500 * i + 1);
     });
-
-    $('.jumbotron h1').css({
-
-        'transform' : 'translate(0px, '+ Wscroll/2 +'%)'
-    });
-
-    $('.jumbotron p').css({
-
-        'transform' : 'translate(0px, '+ Wscroll/1.2+'%)'
-    });
-    // portfolio
-
-    if( Wscroll > $('.portfolio').offset().top -250){
-        $('.portfolio .thumbnail').each(function(i){
-            setTimeout(function(){
-                $('.portfolio .thumbnail').eq(i).addClass('muncul');
-
-            }, 500 * i+1);
-
-        });
-        
-        
-    }
-
+  }
 });
