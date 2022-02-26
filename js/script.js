57020b527942c4aeb40cd3fe9f9b6b0b294be9f1
muhammad iqbal <muhammadiqbal16003@gmail.com>
2022-02-26 19:32:40 +0700

    update modal img

--- js/script.js
+++ js/script.js
@@ -47,3 +47,40 @@ $(window).scroll(function () {
     });
   }
 });
+const container = document.querySelector('.portfolio');
+
+const modal = document.getElementById('myModal');
+const modalImg = document.getElementById('img01');
+const captionText = document.getElementById('caption');
+const span = document.getElementsByClassName('close')[0];
+
+container.addEventListener('click', function (e) {
+  if (e.target.id == 'myImg') {
+    modal.style.display = 'block';
+    modalImg.src = e.target.src;
+    captionText.innerHTML = e.target.alt;
+  }
+});
+span.onclick = function () {
+  modal.style.display = 'none';
+};
+// // Get the modal
+// const modal = document.getElementById('myModal');
+
+// // Get the image and insert it inside the modal - use its "alt" text as a caption
+// const img = document.getElementById('myImg');
+// const modalImg = document.getElementById('img01');
+// const captionText = document.getElementById('caption');
+// img.onclick = function () {
+//   modal.style.display = 'block';
+//   modalImg.src = this.src;
+//   captionText.innerHTML = this.alt;
+// };
+
+// // Get the <span> element that closes the modal
+// const span = document.getElementsByClassName('close')[0];
+
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function () {
+//   modal.style.display = 'none';
+// };
